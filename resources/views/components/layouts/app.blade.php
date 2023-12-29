e1749f29927b9e0361f0504177ef993bb4fffd26
Awodumila, Tobi Ebenezer <tobi@mail.com>
2023-12-29 09:31:36 +0100

    add style and script derivative to live wire

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,16 +8,19 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
     <title>{{ $title ?? 'Writer' }}</title>
+    @livewireStyles
 </head>
 <Style>
-    body{
+    body {
         background-color: #e2e2e2;
     }
 </Style>
-<body >
+
+<body>
     {{ $slot }}
 </body>
 
 <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+@livewireScripts
 
 </html>
